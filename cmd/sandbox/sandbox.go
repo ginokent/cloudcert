9d1867d5fbcc57ae92b55b870546a816c4298980
ginokent <29125616+ginokent@users.noreply.github.com>
2022-09-19 00:15:30 +0900

    wwwwwwip

--- cmd/sandbox/sandbox.go
+++ cmd/sandbox/sandbox.go
@@ -4,8 +4,38 @@ import (
 	"log"
 
 	cloudcertv1 "github.com/ginokent/cloudcert/generated/go/proto/v1/cloudcert"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/reflect/protoreflect"
+	"google.golang.org/protobuf/runtime/protoimpl"
 )
 
 func main() {
-	log.Printf("%#v\n", cloudcertv1.DNSProvider_DNS_PROVIDER_GCLOUD.Descriptor())
+	log.Printf(
+		"%#v\n",
+		proto.GetExtension(
+			cloudcertv1.DNSProvider_DNS_PROVIDER_GCLOUD.Descriptor().Values().Get(int(cloudcertv1.DNSProvider_DNS_PROVIDER_GCLOUD)).Options(),
+			cloudcertv1.E_EnumStringer,
+		),
+	)
+
+	log.Printf(
+		"%s\n",
+		ProtoEnumString(cloudcertv1.DNSProvider_DNS_PROVIDER_GCLOUD, cloudcertv1.E_EnumStringer),
+	)
+}
+
+type ProtoEnumStringer interface {
+	~int32
+	Descriptor() protoreflect.EnumDescriptor
+	String() string
+}
+
+func ProtoEnumString[T ProtoEnumStringer](d T, info *protoimpl.ExtensionInfo) string {
+	ext, ok := proto.GetExtension(d.Descriptor().Values().Get(int(d)).Options(), info).(string)
+
+	if !ok {
+		return d.String()
+	}
+
+	return ext
 }
